eceece71ee2d469b452cf475c87113aeb4ad5d37
Gozi1 <snwabiani@gmail.com>
2022-12-04 22:25:43 +0000

    query :5. currently enrolled students' average assignment completion time.

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT students.name as student,  AVG(duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+WHERE end_date IS NULL 
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
