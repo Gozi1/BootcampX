9ce6e1f5b2d0b56fbe11f4ec3c2017a5d20d5b37
Gozi1 <snwabiani@gmail.com>
2022-12-19 12:11:33 +0000

    query for finding all teachers that made an assistance request during a cohort in js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher ,cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id 
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
