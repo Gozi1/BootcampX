da2524d5aea679ba15ef4367e61d9ae3d2c92d49
Gozi1 <snwabiani@gmail.com>
2022-12-19 12:36:00 +0000

    updated teachers.js and students.js to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -5,16 +5,19 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher ,cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+  `;
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName|| 'JUL02'}`];
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
